616fa22fa5cbdbc752e1cf050df2af1680e9931c
ANONYMUMUS <120654240+ANONYMUMUS@users.noreply.github.com>
2025-12-23 00:31:27 +0800

    package.js

--- package.js
+++ package.js
@@ -2,6 +2,10 @@
   "name": "sosix-server",
   "version": "1.0.0",
   "main": "server.js",
-  "dependencies": { "express": "^4.18.2" },
-  "scripts": { "start": "node server.js" }
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "scripts": {
+    "start": "node server.js"
+  }
 }
